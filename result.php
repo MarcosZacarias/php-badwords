bd80d30ebbddac212600b5baf19153f0087bea1d
MarcosZacarias <marcoszacariasdaniel@gmail.com>
2023-09-25 15:49:43 +0200

    add files and structure

---
+++ result.php
@@ -0,0 +1,24 @@
+<?php 
+
+$censored_word = $_GET["censored_word"];
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+
+</head>
+<body>
+<div class="container">
+      <h1><?php echo $censored_word?></h1>
+      
+    </div>
+ 
+
+</body>
+</html>
